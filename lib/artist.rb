7905dc59db2e835bbfc4880a841356c95aedb937
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-01 18:45:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,7 @@
 class Artist
-  attr_accessor :name, :genre
+  attr_accessor :name
  
-  def initialize(name, genre) 
+  def initialize(name
     @name = name
-    @genre = genre
-  end
- 
-end
+  end 
+end 
